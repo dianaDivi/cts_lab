db9a80c496a56119920bbf4ba11749133505544c
Diana <dugan_diana@yahoo.ro>
2021-06-05 14:43:10 +0300

    Final

--- seminar_7/src/ro/ase/CTS/adapter/EuropeanCar.java
+++ seminar_7/src/ro/ase/CTS/adapter/EuropeanCar.java
@@ -1,5 +1,12 @@
 package ro.ase.CTS.adapter;
 
 public class EuropeanCar {
+	private String model;
 
+	public EuropeanCar(String model) {
+		super();
+		this.model = model;
+	}
+
+	public abstract int getMaxSpeed();
 }
